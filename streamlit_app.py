2ce65bab515b32ffff0a10a1b0835ede1c33dc36
Harshith-Varma27 <harshithvarmanadimpalli27@gmail.com>
2026-01-08 16:05:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,15 +30,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
 
+    smoothiefroot_response = requests.get(
+        "https://my.smoothiefroot.com/api/fruit/watermelon"
+    )
 
-sf_df = st.dataframe(
-    data=smoothiefroot_response.json(),
-    use_container_width=True
-)
+    sf_df = st.dataframe(
+        data=smoothiefroot_response.json(),
+        use_container_width=True
+    )
+
 
     # Build INSERT statement  ✅ (FIXED INDENTATION)
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
